e5f9714af50c36e103d0401d73b7f17f2a386ebd
jfeng1115 <jfeng1115@gmail.com>
2015-02-03 13:07:25 -0500

    added tests for my app

---
+++ application/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Food Truck',
+      version='1.0',
+      description='Food Truck Finder',
+      author='Jie Feng',
+      author_email='jfeng1115@gmail.com',
+      url='',
+     install_requires=['Flask>=0.10.1', 'MarkupSafe' , 'Flask-SQLAlchemy>=1.0'],
+     )
