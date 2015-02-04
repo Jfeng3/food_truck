27cc5e4c451061381c90d33d393539c0122e0a87
jfeng1115 <jfeng1115@gmail.com>
2015-02-03 22:53:21 -0500

    added some test cases for get_nearest_truck and trucks_within

--- application/test_app.py
+++ application/test_app.py
@@ -1,75 +1,36 @@
 # -*- coding: utf-8 -*-
 """
-    Flaskr Tests
+    map Tests
     ~~~~~~~~~~~~
-    Tests the Flaskr application.
-    :copyright: (c) 2015 by Armin Ronacher.
+    Tests the map application.
+    :copyright: (c) 2015 by Jie Feng
     :license: BSD, see LICENSE for more details.
 """
-
 import pytest
-
 import os
-import flaskr
-import tempfile
-
-
+import map
+import json
 @pytest.fixture
 def client(request):
-    db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-    flaskr.app.config['TESTING'] = True
-    client = flaskr.app.test_client()
-    with flaskr.app.app_context():
-        flaskr.init_db()
-
+    map.app.config['TESTING'] = True
+    map.app.config['TABLE_NAME'] = "test_food_truck"
+    client = map.app.test_client()
+    with map.app.app_context():
+        map.init_db()
     def teardown():
-        os.close(db_fd)
-        os.unlink(flaskr.app.config['DATABASE'])
+        pass
     request.addfinalizer(teardown)
-
     return client
-
-
-def login(client, username, password):
-    return client.post('/login', data=dict(
-        username=username,
-        password=password
-    ), follow_redirects=True)
-
-
-def logout(client):
-    return client.get('/logout', follow_redirects=True)
-
-
-def test_empty_db(client):
-    """Start with a blank database."""
-    rv = client.get('/')
-    assert b'No entries here so far' in rv.data
-
-
-def test_login_logout(client):
-    """Make sure login and logout works"""
-    rv = login(client, flaskr.app.config['USERNAME'],
-               flaskr.app.config['PASSWORD'])
-    assert b'You were logged in' in rv.data
-    rv = logout(client)
-    assert b'You were logged out' in rv.data
-    rv = login(client, flaskr.app.config['USERNAME'] + 'x',
-               flaskr.app.config['PASSWORD'])
-    assert b'Invalid username' in rv.data
-    rv = login(client, flaskr.app.config['USERNAME'],
-               flaskr.app.config['PASSWORD'] + 'x')
-    assert b'Invalid password' in rv.data
-
-
-def test_messages(client):
-    """Test that messages work"""
-    login(client, flaskr.app.config['USERNAME'],
-          flaskr.app.config['PASSWORD'])
-    rv = client.post('/add', data=dict(
-        title='<Hello>',
-        text='<strong>HTML</strong> allowed here'
-    ), follow_redirects=True)
-    assert b'No entries here so far' not in rv.data
-    assert b'&lt;Hello&gt;' in rv.data
-    assert b'<strong>HTML</strong> allowed here' in rv.data
+    
+def test_get_trucks(client):
+    """test get nearest truck location"""
+    rv = client.get('/trucks?lat=37.790149087496&lon=-122.39865818459')
+    truck_info = json.loads(rv.data)
+    assert truck_info[0]['longitude'] == -122.390016333636
+    
+def test_trucks_within(client):
+    """test trucks with a rectangle"""
+    rv = client.get('/trucks/within?lat1=37.78658909826544&lon1=-122.40637421607971&lat2=37.793795881958815&lon2=-122.39244818687439')
+    truck_info = json.loads(rv.data)
+    assert len(truck_info) == 25
+    assert truck_info[0]['longitude'] == -122.394593199235
